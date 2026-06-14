2f33e42502e067f57cb2b523d713cc296a868b38
Gabriel Felipe Kuhn Schiestl <gabriel.schiestl25@hotmail.com>
2026-06-14 19:39:20 -0300

    feat: add endpoint metrics aggregation

--- packages/server/internal/runner/aggregator.go
+++ packages/server/internal/runner/aggregator.go
@@ -12,6 +12,7 @@ func aggregateData(records []JTLRecord) services.AggregatedData {
 	p90Ms := calculatePercentile(records, 90)
 	p99Ms := calculatePercentile(records, 99)
 	errorsGrouped := groupErrors(records)
+	endpointMetrics := calculateEndpointMetrics(records)
 
 	return services.AggregatedData{
 		TotalRequests: len(records),
@@ -20,6 +21,7 @@ func aggregateData(records []JTLRecord) services.AggregatedData {
 		LatencyP90Ms: p90Ms,
 		LatencyP99Ms: p99Ms,
 		ErrorsByType: errorsGrouped,
+		EndpointMetrics: endpointMetrics,
 	}
 }
 
@@ -74,4 +76,33 @@ func groupErrors(records []JTLRecord) []services.ErrorGroup {
 	}
 
 	return errorGroups
+}
+
+func calculateEndpointMetrics(records []JTLRecord) []services.EndpointMetric {
+	groupedEndpoints := make(map[string][]JTLRecord)
+	for _, rec := range records {
+		if _, ok := groupedEndpoints[rec.Label]; !ok {
+			groupedEndpoints[rec.Label] = []JTLRecord{}
+		}
+		groupedEndpoints[rec.Label] = append(groupedEndpoints[rec.Label], rec)
+	}
+
+	results := make([]services.EndpointMetric, len(groupedEndpoints))
+	for _, records := range groupedEndpoints {
+		results = append(results, aggregateEndpointData(records))
+	}
+
+	return results
+}
+
+func aggregateEndpointData(records []JTLRecord) (res services.EndpointMetric) {
+	res.Label = records[0].Label
+	res.TotalCalls = len(records)
+	res.ErrorRate = calculateErrorRate(records)
+
+	res.P50Ms = calculatePercentile(records, 50)
+	res.P90Ms = calculatePercentile(records, 90)
+	res.P99Ms = calculatePercentile(records, 99)
+
+	return
 }
